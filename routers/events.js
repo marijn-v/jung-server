1818519fbc853ddc0475050b93175e33640c6bbe
marijn-v <marijnverbiesen@gmail.com>
2021-10-14 13:26:44 +0200

    post event route

--- routers/events.js
+++ routers/events.js
@@ -1,8 +1,8 @@
 const { Router } = require("express");
-// const auth = require("../auth/middleware");
+const auth = require("../auth/middleware");
 
 const Event = require("../models").event;
-// const User = require("../models").user;
+const User = require("../models").user;
 const Venue = require("../models").venue;
 
 const router = new Router();
@@ -15,6 +15,35 @@ router.get("/", async (req, res, next) => {
       include: [{ model: Venue }],
     });
     res.send(allEvents);
+  } catch (e) {
+    next(e);
+  }
+});
+
+// add event + ADD AUTH
+router.post("/:id/add", auth, async (req, res, next) => {
+  const user = await User.findByPk(req.params.id);
+  console.log("user in backend", user);
+
+  try {
+    const { title, image, date, description, link, venueId } = req.body;
+
+    if (!title || !image || !date || !description) {
+      return res
+        .status(400)
+        .send("Please provide title, image url, date and description");
+    } else {
+      const newEvent = await Event.create({
+        title,
+        image,
+        date,
+        description,
+        link,
+        userId: user.id, // passing it as params?
+        venueId, // how to add this..
+      });
+      res.json(newEvent);
+    }
   } catch (e) {
     next(e);
   }
