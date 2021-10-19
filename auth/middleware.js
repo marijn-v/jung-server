ed7e8ba3ad39c76496a3aba51db94aa11ffea241
marijn-v <marijnverbiesen@gmail.com>
2021-10-19 09:23:57 +0200

    added event to /me

--- auth/middleware.js
+++ auth/middleware.js
@@ -1,5 +1,6 @@
 const User = require("../models").user;
 const { toData } = require("./jwt");
+const Event = require("../models").event;
 
 async function auth(req, res, next) {
   const auth =
@@ -14,7 +15,9 @@ async function auth(req, res, next) {
 
   try {
     const data = toData(auth[1]);
-    const user = await User.findByPk(data.userId);
+    const user = await User.findByPk(data.userId, {
+      include: [{ model: Event, as: "attending" }],
+    });
     if (!user) {
       return res.status(404).send({ message: "User does not exist" });
     }
